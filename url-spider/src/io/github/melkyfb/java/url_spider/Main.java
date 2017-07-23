b26d1384bbcc69fc0c585603edfea7449f30c4d5
Melky Salem Fernandes Batista da Silva <melksalem@gmail.com>
2017-07-23 00:42:50 -0300

    continue the project. parsing the variables.

--- url-spider/src/io/github/melkyfb/java/url_spider/Main.java
+++ url-spider/src/io/github/melkyfb/java/url_spider/Main.java
@@ -1,9 +1,11 @@
 package io.github.melkyfb.java.url_spider;
 
+import java.io.PrintStream;
+
 import org.apache.commons.validator.routines.UrlValidator;
 
 public class Main {
-
+	private static PrintStream out = System.out;
 	public static void main(String[] args) {		
 		boolean show_usage = args.length == 0 || args[0] == "-h" || args[0] == "--help";
 		if(show_usage) {
@@ -11,27 +13,41 @@ public class Main {
 		} else {
 			UrlValidator urlValidator = new UrlValidator();
 			if(urlValidator.isValid(args[args.length-1])) {
-				
+				int mdepth = 3;
+				String opath = "";
+				for(String arg : args) {
+					if(arg.contains("--maxdepth=")) mdepth = Integer.parseInt(arg.replace("--maxdepth=", ""));
+					if(arg.contains("-o=")) opath = arg.replace("-o=", "");
+					if(arg.contains("--output=")) opath = arg.replace("--output=", "");
+				}
+				String surl = args[args.length-1];
+				out.println("JAVA URL Spider++++++++++++++++++++++++++++");
+				out.println("wrote by melkysalem at gmail dot com\n\n");
+				out.println("URL Spider Started...");
+				out.println("URL: "+surl);		
+				out.println("Max Depth: "+mdepth);	
+				if(opath.length()>0) out.println("Output Path: "+opath);
+				else out.println("Output to console");
 			} else {
-				System.out.println(args[args.length-1]+" is not a valid url, please check that and try again\n");
-				System.out.println("use only -h or --help argument to show usage\n");
+				out.println(args[args.length-1]+" is not a valid url, please check that and try again");
+				out.println("use only -h or --help argument to show usage");
 			}
 		}
 	}
 	private static void showUsage() {
-		String usage = "URL Spider - A Web Spider\n";
-		usage 		+= "Usage:\n";
-		usage		+= "\n";
-		usage		+= "java -jar url_spider.jar <options> url\n";
-		usage		+= "\n";
-		usage		+= "Options:\n";
-		usage		+= "-maxdepth=N (N default=3) catch N depth for url\n";
-		usage		+= "-o (Default to output) write to an output file \n";
-		usage		+= "-h --help show this usage\n";
-		usage		+= "\n";
-		usage		+= "wrote by melkysalem at gmail dot com\n";
-		usage		+= "https://melkyfb.github.io/\n";
-		System.out.println(usage);
+		String usage = "URL Spider - A Web Spider";
+		usage 		+= "Usage:";
+		usage		+= "";
+		usage		+= "java -jar url_spider.jar <options> url";
+		usage		+= "";
+		usage		+= "Options:";
+		usage		+= "--maxdepth=N (N default=3) catch N depth for url";
+		usage		+= "-o=PATH --output=PATH (Default to console) write to an output file ";
+		usage		+= "-h --help show this usage";
+		usage		+= "";
+		usage		+= "wrote by melkysalem at gmail dot com";
+		usage		+= "https://melkyfb.github.io/";
+		out.println(usage);
 	}
 
 }
